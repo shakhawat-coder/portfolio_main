e332ff283deb5d1dddd40a728d4889584cac0b71
shakhawat <shakhawathossen188@gmail.com>
2023-08-25 20:57:51 +0600

    home section design

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(function(){
+    // var typed = new Typed(".typed", {
+    //     strings: ["Shakhawat"],
+    //     typeSpeed: 100,
+    //     backSpeed: 100,
+    //     backDelay: 3000,
+    //     showCursor: false,
+    //     loop:true
+    //   });
+
+      var typed = new Typed(".typed", {
+        strings: ["Frontend Developer","Professional coder"],
+        typeSpeed: 80,
+        backSpeed: 30,
+        showCursor: false,
+        // fadeOutClass: 'typed-fade-out',
+        fadeOutDelay: 2000,
+        loop:true
+      });
+
+      jQuery('.skillbar').each(function(){
+        jQuery(this).find('.skillbar-bar').animate({
+            width:jQuery(this).attr('data-percent')
+        },2000);
+    });
+});
