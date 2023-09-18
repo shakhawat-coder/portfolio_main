9ca5244931b619907af337227d4e7191a46ebbb4
shakhawat <shakhawathossen188@gmail.com>
2023-09-18 20:24:59 +0600

    fix some issue

--- js/script.js
+++ js/script.js
@@ -1,13 +1,4 @@
 $(function(){
-    // var typed = new Typed(".typed", {
-    //     strings: ["Shakhawat"],
-    //     typeSpeed: 100,
-    //     backSpeed: 100,
-    //     backDelay: 3000,
-    //     showCursor: false,
-    //     loop:true
-    //   });
-
       var typed = new Typed(".typed", {
         strings: ["Frontend Developer","Professional coder"],
         typeSpeed: 80,
@@ -24,11 +15,6 @@ $(function(){
     //   'wrapAround': true
     // })
 
-      jQuery('.skillbar').each(function(){
-        jQuery(this).find('.skillbar-bar').animate({
-            width:jQuery(this).attr('data-percent')
-        },2000);
-    });
 });
 
 // =================back to top button=============
